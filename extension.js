cd0fbcc4b3861f046b47f262e80ed803b8e44f2e
chunyang <chunyang.chen@iglooinsure.com>
2021-12-15 18:21:13 +0800

    feat: update

--- extension.js
+++ extension.js
@@ -12,6 +12,13 @@ function activate(context) {
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with  registerCommand
   // The commandId parameter must match the command field in package.json
+
+  // 获取正则前面是fm还是formatMessage
+  const formatKey = vscode.workspace
+    .getConfiguration()
+    .get("conversion.transOpts");
+
+  // 两边不加花括号版本
   let disposable = vscode.commands.registerCommand(
     "QuickConversion.change",
     function () {
@@ -27,14 +34,47 @@ function activate(context) {
       editor.edit((editBuilder) => {
         // 循环遍历替换
         selections.forEach((selection) => {
-          const text = document.getText(selection);
-          editBuilder.replace(selection, `fm({id: '${text}'})`);
+          let text = document.getText(selection);
+          // 清除左右的中括号
+          text = text.replace(/^{(.*)}$/g, "$1");
+          // 清除左右的引号
+          text = text.replace(/^["|'](.*)["|']$/g, "$1");
+          // 替换为新的字符串
+          editBuilder.replace(selection, `${formatKey}({id: '${text}'})`);
         });
       });
     }
   );
 
-  context.subscriptions.push(disposable);
+  // 两边加花括号版本
+  let disposable2 = vscode.commands.registerCommand(
+    "QuickConversion.change2",
+    function () {
+      const editor = vscode.window.activeTextEditor;
+
+      if (!editor) {
+        return;
+      }
+
+      const document = editor.document;
+      const selections = editor.selections;
+
+      editor.edit((editBuilder) => {
+        // 循环遍历替换
+        selections.forEach((selection) => {
+          let text = document.getText(selection);
+          // 清除左右的中括号
+          text = text.replace(/^{(.*)}$/g, "$1");
+          // 清除左右的引号
+          text = text.replace(/^["|'](.*)["|']$/g, "$1");
+          // 替换为新的字符串
+          editBuilder.replace(selection, `{${formatKey}({id: '${text}'})}`);
+        });
+      });
+    }
+  );
+
+  context.subscriptions.push(disposable, disposable2);
 }
 
 // this method is called when your extension is deactivated
